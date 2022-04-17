c9b353ae677ccccd8094bd252486dfd231ad3ae3
milan.langeler <milan.langeler@gmail.com>
2022-04-17 18:36:07 +0200

    Initial commit

---
+++ Graph.py
@@ -0,0 +1,16 @@
+class Graph:
+    taxi = ["taxi"]
+    bus = ["bus"]
+    underground = ["underground"]
+    taxi_bus = ["taxi", "bus"]
+    bus_underground = ["bus", "underground"]
+
+    def build_graph(self):
+        x = "hallo"
+
+
+
+
+
+    def add_edge(self, departure, destination, travel_option):
+        x = "hallo"
